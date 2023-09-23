5abb133c08db68056d08794240eed58401079fd7
BourhaneYounes <bourhaneyounes1@gmail.com>
2023-09-23 14:06:11 +0100

    fixed path errors, import base from app/paths

--- src/components/pokeCard/PokeCard.svelte
+++ src/components/pokeCard/PokeCard.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+    import { base } from "$app/paths";
     export let pokemon: any
 </script>
 
-<a href={`/pokemon/${pokemon.id}`}>
+<a href={`${base}/pokemon/${pokemon.id}`}>
     <div class="p-4 bg-custom-green text-gray-800 text-center rounded-md shadow hover:shadow-xl flex flex-col items-center">
         <p class="id">#{pokemon.id}</p>
         <img src={pokemon.image} alt={pokemon.name}>
